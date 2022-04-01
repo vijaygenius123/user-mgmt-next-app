369a3567649c3a3aa6d148568178053549cb79dc
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-04-01 20:18:27 +0100

    Add tailwind and Navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return <div className="bg-gray-800">
+        <div className="h-16 px-8 flex items-center">
+            <p className="text-white font-bold">User Management System</p>
+        </div>
+    </div>
+}
+
+export default Navbar
